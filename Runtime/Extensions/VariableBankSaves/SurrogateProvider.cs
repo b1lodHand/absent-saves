327e9ffaf6a0d0a7611be4c815e7b301afd09a81
absencee_ <ilyasofli06@gmail.com>
2024-10-28 15:00:50 +0300

    import vb saves

---
+++ Runtime/Extensions/VariableBankSaves/SurrogateProvider.cs
@@ -0,0 +1,19 @@
+#if CAN_SAVE_VB
+
+using System.Runtime.Serialization;
+
+namespace com.absence.savesystem.variablebanks
+{
+    static class SurrogateProvider
+    {
+        [SurrogateProviderMethod("Provider for VariableBanks")]
+        public static void Provide(SurrogateSelector targetSelector)
+        {
+            VariableBankSerializationSurrogate variableBankSerializationSurrogate = new VariableBankSerializationSurrogate();
+            targetSelector.AddSurrogate(typeof(VariableBankSerializationSurrogate), new StreamingContext(StreamingContextStates.All), variableBankSerializationSurrogate);
+        }
+    }
+
+}
+
+#endif
---
+++ Runtime/Extensions/VariableBankSaves/TempVariableBankData.cs
@@ -0,0 +1,42 @@
+#if CAN_SAVE_VB
+
+using com.absence.variablesystem;
+using com.absence.variablesystem.builtin;
+using System.Collections.Generic;
+
+namespace com.absence.savesystem.variablebanks
+{
+    [System.Serializable]
+    public class TempVariableBankData
+    {
+        public int IntCount = 0;
+        public int FloatCount = 0;
+        public int StringCount = 0;
+        public int BooleanCount = 0;
+
+        public List<Integer> Ints = new();
+        public List<Float> Floats = new();
+        public List<String> Strings = new();
+        public List<Boolean> Booleans = new();
+
+        public TempVariableBankData()
+        {
+        }
+
+        public TempVariableBankData(VariableBank copyFrom)
+        {
+            Ints = new(copyFrom.Ints);
+            Floats = new(copyFrom.Floats);
+            Strings = new(copyFrom.Strings);
+            Booleans = new(copyFrom.Booleans);
+
+            IntCount = Ints.Count;
+            FloatCount = Floats.Count;
+            StringCount = Strings.Count;
+            BooleanCount = Booleans.Count;
+        }
+    }
+
+}
+
+#endif
